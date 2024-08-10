605add4a9822fb70cefae355fbd251190bd1c048
Marco Perez <108315533+VinicioPerez@users.noreply.github.com>
2024-08-10 15:50:24 -0500

    instalando lo themes de formkit y su config para que su apariencia sea mejor

--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -10,14 +10,62 @@ import Link from '@/components/Link.vue';
         >Volver</Link>
         <h1 class="text-4xl font-black my-10">Nuevo Producto</h1>
 
-        <FormKit
-            type="form"
-        >
-            <FormKit
-                type="text"
-            />
+        <div class="flex justify-center bg-white shadow">
+            <div class="mt-10 p-10 w-full 2xl:w-2/4">
+                
+                <FormKit
+                    type="form"
+                    submit-label="Agregar Producto"
+                >
+                    <FormKit
+                        type="text"
+                        label="Nombre del Producto"
+                        name="name"
+                        placeholder="Ingrese el nombre del producto"
+                        validation="required"
+                        :validation-messages="{required: 'El nombre del producto es obligatorio'}"
+                    />
 
-        </FormKit>
+                    <FormKit
+                        type="file"
+                        label="Imagen Producto"
+                        name="image"
+                        validation="required"
+                        :validation-messages="{required: 'La imagen del producto es obligatoria'}"
+                        accept=".jpg, .png"
+                    />
+
+                    <FormKit
+                        type="select"
+                        label="Categoria"
+                        name="category"
+                        validation="required"
+                        :validation-messages="{required: 'La categoria del producto es obligatoria'}"
+                        :options="[1,2,3,4,5]"
+                    />
+                    <FormKit
+                        type="number"
+                        label="Precio"
+                        name="price"
+                        placeholder="Precio del Producto"
+                        validation="required"
+                        :validation-messages="{required: 'El Precio del producto es obligatoria'}" 
+                        min="1"
+                    />
+                    <FormKit
+                        type="number"
+                        label="Disponibles"
+                        name="availabity"
+                        placeholder="Cantidad disponible"
+                        validation="required"
+                        :validation-messages="{required: 'La cantidad es obligatoria'}" 
+                        min="1"
+                    />
+                
+                </FormKit>
+
+            </div>
+        </div>
 
 
     </div>
