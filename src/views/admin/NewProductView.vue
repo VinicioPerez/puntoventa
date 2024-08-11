75248bc84bbec2ba49ced36ab175f591f73379e1
Marco Perez <108315533+VinicioPerez@users.noreply.github.com>
2024-08-10 22:53:21 -0500

    configurando para usar la firebase y agregando productos a la base de datos con la imagen, guardando la URL de la imagen

--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -1,5 +1,37 @@
 <script setup>
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
 import Link from '@/components/Link.vue';
+import useImage from '@/composables/useImage'
+import { useProductsStore } from '@/stores/products';
+
+const {url, onFileChange, isImageUploaded } = useImage()
+const  products  = useProductsStore()
+const router = useRouter()
+
+const formData = reactive({
+    name: '',
+    category: '',
+    price: '',
+    availability: '',
+    image: ''
+})
+
+const submitHandler = async data => {
+    const { image, ...values} = data
+
+    try {
+        await products.createProduct({
+            ...values,
+            image: url.value
+        })    
+        router.push({name: 'products'})    
+    } catch (error) {
+        console.error(error)        
+    }
+
+    
+}
 
 </script>
 
@@ -16,6 +48,10 @@ import Link from '@/components/Link.vue';
                 <FormKit
                     type="form"
                     submit-label="Agregar Producto"
+                    incomplete-message="No se puedo enviar, revisa los mensajes"
+                    @submit="submitHandler"
+                    :value="formData"
+
                 >
                     <FormKit
                         type="text"
@@ -24,6 +60,7 @@ import Link from '@/components/Link.vue';
                         placeholder="Ingrese el nombre del producto"
                         validation="required"
                         :validation-messages="{required: 'El nombre del producto es obligatorio'}"
+                        v-model.trim="formData.name"
                     />
 
                     <FormKit
@@ -33,7 +70,17 @@ import Link from '@/components/Link.vue';
                         validation="required"
                         :validation-messages="{required: 'La imagen del producto es obligatoria'}"
                         accept=".jpg, .png"
+                        @change="onFileChange"
+                        v-model.trim="formData.image"
                     />
+                    <div v-if="isImageUploaded">
+                        <p>Imagen Producto:</p>
+                        <img 
+                            :src="isImageUploaded" 
+                            alt="Nueva imagen producto"
+                            class="w-32"
+                        >
+                    </div>
 
                     <FormKit
                         type="select"
@@ -41,7 +88,8 @@ import Link from '@/components/Link.vue';
                         name="category"
                         validation="required"
                         :validation-messages="{required: 'La categoria del producto es obligatoria'}"
-                        :options="[1,2,3,4,5]"
+                        :options="products.categoryOptions"
+                        v-model.number="formData.category"
                     />
                     <FormKit
                         type="number"
@@ -51,15 +99,17 @@ import Link from '@/components/Link.vue';
                         validation="required"
                         :validation-messages="{required: 'El Precio del producto es obligatoria'}" 
                         min="1"
+                         v-model.number="formData.price"
                     />
                     <FormKit
                         type="number"
                         label="Disponibles"
-                        name="availabity"
+                        name="availability"
                         placeholder="Cantidad disponible"
                         validation="required"
                         :validation-messages="{required: 'La cantidad es obligatoria'}" 
                         min="1"
+                         v-model.number="formData.availability"
                     />
                 
                 </FormKit>
