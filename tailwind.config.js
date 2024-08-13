63fc6b5ddb84e67e6c39dbc65a1afe192b2a519b
Marco Perez <108315533+VinicioPerez@users.noreply.github.com>
2024-08-13 15:01:55 -0500

    finalizando el proyecto con la implementacion de la s ventas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./formkit.config.js"
+    "./formkit.config.js",
+    "./node_modules/vue-tailwind-datepicker/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'vtd-primary': colors.green,
+        // 'secondary': colors.gray[500],
+        // 'success': colors.green[500],
+        // 'error': colors.red[500],
+        // 'warning': colors.yellow[500],
+        // 'info': colors.blue[500],
+        // 'dark': colors.gray[800],
+        // 'light': colors.gray[100],
+        // 'white': colors.white,
+        // 'black': colors.black,
+        // 'gray': colors.gray,
+      }
+    },
   },
   plugins: [
     require("@tailwindcss/forms"),
